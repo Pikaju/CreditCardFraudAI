1408bf9e4bee7ce74331f3da8c05835e6eab32b9
Julien Scholz <julienscholzo@hotmail.de>
2019-11-27 21:48:04 +0100

    Fix layer names

--- src/mlp.py
+++ src/mlp.py
@@ -6,13 +6,13 @@ from tensorflow.keras import layers
 class FraudDetectionMLP(keras.Model):
     def __init__(self):
         super(FraudDetectionMLP, self).__init__()
-        self.dense3 = layers.Dense(64, activation=tf.nn.relu)
-        self.dense4 = layers.Dense(1, activation=tf.nn.sigmoid)
+        self.dense1 = layers.Dense(64, activation=tf.nn.relu)
+        self.dense2 = layers.Dense(1, activation=tf.nn.sigmoid)
         self.dropout = layers.Dropout(0.3)
 
     def call(self, inputs, training=False):
         x = inputs
-        x = self.dense3(x)
+        x = self.dense1(x)
         x = self.dropout(x, training=training)
-        x = self.dense4(x)
+        x = self.dense2(x)
         return x
